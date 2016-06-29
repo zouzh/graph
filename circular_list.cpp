e36d3e6825f05980e7e2b1e7214e9a2e0dbc0d84
zouzh <1179823185@qq.com>
2016-06-29 22:38:38 +0800

    Construct circular list, and resolve Josephus problem

---
+++ circular_list.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+struct node
+{ int item; node* next;
+node(int x, node* t)
+{ item = x; next = t; }
+};
+typedef node *link;
+int main(int argc, char *argv[])
+{
+  using namespace std;
+  int i, N = atoi(argv[1]), M = atoi(argv[2]);
+  link t = new node(1, 0); t->next = t;
+  link x = t;
+  for (i = 2; i <= N; i++)
+    x = (x->next = new node(i, t));
+  while (x != x->next)
+    {
+      for (i = 1; i < M; i++) x = x->next;
+	x->next = x->next->next;
+    }
+  cout << x->item << endl;
+}
+# C-C-
