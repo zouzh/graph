986b9c800fa13b6ed22fc3c43128a7dbcf34818f
zouzh <1179823185@qq.com>
2016-06-29 21:54:56 +0800

    Construct array whose elements are pointers

--- pointer_array.cpp
+++ pointer_array.cpp
@@ -1,2 +1,23 @@
-# C-C-
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void output(int *[],int *, int);
+int main(int argc,char * argv[])
+{
+  int m=atoi(argv[1]);
+  int *p[m];
+  int *q = new int [m];
+  output(p,q,m);
+  delete [] q;
+  return 0;
+}
+void output(int *p[],int *q, int m)
+{
+  
+  for(int i=0;i<m;++i)
+    p[i] = &q[i];
+  for(int i=0;i<m;++i)
+    cout << p[i] << "  ";
+  cout<<endl;
+}
 # C-C-
